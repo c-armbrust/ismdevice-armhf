48b7232dd4d95a42ec11e99b021aadf2fc078f2f
Leo <leoreentry@protonmail.com>
2017-01-17 11:19:29 +0100

    Update script

--- generate.sh
+++ generate.sh
@@ -6,6 +6,16 @@
 #        IdentityFile /root/.ssh/id_ed25519 # Your ssh key to authenticate if you placed the public part in ~/.ssh/authorized_keys
 echo "Building project..."
 make
+
+if [ $? -ne 0 ]; then
+  echo -e "\nAn error occured during compilation." 1>&2
+  exit 1
+fi
+chown 1000:1000 statetest
 echo "Copying executable to BBB..."
 scp statetest bbb:~
+if [ $? -ne 0 ]; then
+  echo -e "\nAn error occured during copying." 1>&2
+  exit 1
+fi
 echo "Done!"

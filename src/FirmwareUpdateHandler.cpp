f8243a230ed41bf22126660cba38dff1772b7b30
Leo <leoreentry@protonmail.com>
2017-02-01 12:15:53 +0100

    Copy some blob storage code from camera

--- src/FirmwareUpdateHandler.cpp
+++ src/FirmwareUpdateHandler.cpp
@@ -6,6 +6,13 @@
 #include "iothub_client.h"
 #include "FirmwareUpdateHandler.h"
 
+
+FirmwareUpdateHandler::FirmwareUpdateHandler(utility::string_t stoconnstr, utility::string_t containername, std::string storageaccname) : containerName{containername}, storageAccountName{storageaccname}
+{
+    InitBlobStorage(stoconnstr);
+    memset((void*)stoconnstr.data(), 0, stoconnstr.size());
+}
+
 int FirmwareUpdateHandler::DeviceMethodCallback(const char *method_name, const unsigned char *payload, size_t size, unsigned char **response, size_t *resp_size, void *userContextCallback) {
     (void)userContextCallback;
 
@@ -29,4 +36,33 @@ int FirmwareUpdateHandler::DeviceMethodCallback(const char *method_name, const u
     }
     return status;
 
+}
+
+void FirmwareUpdateHandler::InitBlobStorage(utility::string_t storageConnectionString)
+{
+    try
+    {
+        std::cout << "Create Azure Storage container " << containerName << " on Storage Account " << storageAccountName << " if not exists" << std::endl;
+
+        // Retrieve storage account from connection string.
+        azure::storage::cloud_storage_account storage_account = azure::storage::cloud_storage_account::parse(storageConnectionString);
+
+        // Create the blob client
+        azure::storage::cloud_blob_client blob_client = storage_account.create_cloud_blob_client();
+
+        // Retrieve a reference to a container.
+        this->container = blob_client.get_container_reference(containerName);
+
+        // Create the container if it doesn't already exist.
+        container.create_if_not_exists();
+
+        // Make the blob container publicly accessible.
+        azure::storage::blob_container_permissions permissions;
+        permissions.set_public_access(azure::storage::blob_container_public_access_type::blob);
+        container.upload_permissions(permissions);
+    }
+    catch(const std::exception& e)
+    {
+        std::wcout << U("Error: ") << e.what() << std::endl;
+    }
 }
--- src/FirmwareUpdateHandler.h
+++ src/FirmwareUpdateHandler.h
@@ -5,13 +5,21 @@
 #ifndef ISMDEVICE_FIRMWAREUPDATEHANDLER_H
 #define ISMDEVICE_FIRMWAREUPDATEHANDLER_H
 
+#include "was/storage_account.h"
+#include "was/blob.h"
 
 class FirmwareUpdateHandler {
 public:
+    FirmwareUpdateHandler(utility::string_t, utility::string_t, std::string);
     static int DeviceMethodCallback(const char* method_name, const unsigned char* payload, size_t size, unsigned char** response, size_t* resp_size, void* userContextCallback);
-private:
-    FirmwareUpdateHandler() { };
-};
 
+private:
+    void InitBlobStorage(utility::string_t storageConnectionString);
+    // Blob Storage
+    const utility::string_t containerName;
+    const std::string storageAccountName;
+    azure::storage::cloud_blob_container container;
+
+};
 
 #endif //ISMDEVICE_FIRMWAREUPDATEHANDLER_H
